8e57650efb99048e6675365262c938c4bbbb295f
Mark Gerald Martins <mark.g.martins@gmail.com>
2023-10-19 01:33:08 -0300

    fix

--- chatsocket/chatsocket.go
+++ chatsocket/chatsocket.go
@@ -12,6 +12,7 @@ import (
 	"log"
 	"net/http"
 	"strings"
+	"sync"
 	"time"
 )
 
@@ -30,26 +31,20 @@ var Upgrader = websocket.Upgrader{
 	},
 }
 var mainHub = NewHub()
+var globalHub *Hub = NewHub()
 
 func init() {
-	go mainHub.Run()
+	go globalHub.Run()
 }
 
 var singleHub *Hub
 
-func GetHub() *Hub {
-	if singleHub == nil {
-		singleHub = NewHub()
-		go singleHub.Run()
-	}
-	return singleHub
-}
-
 type Client struct {
-	ID   string
-	Conn *websocket.Conn
-	send chan Message
-	hub  *Hub
+	ID        string
+	Conn      *websocket.Conn
+	send      chan Message
+	hub       *Hub
+	writeLock sync.Mutex
 }
 
 func NewClient(id string, conn *websocket.Conn, hub *Hub) *Client {
@@ -67,10 +62,16 @@ func (c *Client) Read(ctx *gin.Context) {
 	c.Conn.SetPongHandler(func(string) error { c.Conn.SetReadDeadline(time.Now().Add(pongWait)); return nil })
 	for {
 		var msg Message
-		err := c.Conn.ReadJSON(&msg)
+		c.writeLock.Lock()
+		err := c.Conn.WriteJSON(msg)
+		c.writeLock.Unlock()
 		if err != nil {
-			fmt.Println("Error: ", err)
-			break
+			fmt.Printf("Error reading from websocket for client %s: %v", c.ID, err)
+
+			// Tentativa de reconexão ou outras ações aqui...
+
+			c.Close() // Feche a conexão corretamente.
+			return    // Saia da goroutine.
 		}
 		var messageQueue models.Message
 		messageQueue.Username = msg.Sender
@@ -104,10 +105,13 @@ func (c *Client) Write() {
 				c.Conn.WriteMessage(websocket.CloseMessage, []byte{})
 				return
 			} else {
+				c.writeLock.Lock()
 				err := c.Conn.WriteJSON(message)
+				c.writeLock.Unlock()
 				if err != nil {
-					fmt.Println("Error: ", err)
-					break
+					fmt.Printf("Error writing to websocket for client %s: %v", c.ID, err)
+					c.Close()
+					return
 				}
 			}
 		case <-ticker.C:
@@ -139,9 +143,8 @@ func getUpgrader(ctx *gin.Context) *websocket.Conn {
 
 func ServeWS(ctx *gin.Context, roomId string) {
 	ws := getUpgrader(ctx)
-	hub := GetHub()
-	client := NewClient(roomId, ws, hub)
-	hub.register <- client
+	client := NewClient(roomId, ws, globalHub)
+	globalHub.register <- client
 	log.Println("Client registered: " + client.ID)
 	go client.Write()
 	go client.Read(ctx)
